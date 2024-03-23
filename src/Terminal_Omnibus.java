7ee587b2d4422fb5170dbc6f2e0de1e00d320eec
Merychanchan <subarachisekai@gmail.com>
2024-03-23 03:22:59 -0400

    main

--- src/Terminal_Omnibus.java
+++ src/Terminal_Omnibus.java
@@ -1,7 +1,45 @@
 package src;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Terminal_Omnibus {
-    
+
+    private String nombre;
+    private String direccion;
+    float dinero_total = 0;
+    List<String> viajes = new ArrayList<>();
+    List<Lista_Espera> lista_espera = new ArrayList<>();
+    List<Lista_Oficial> lista_oficial = new ArrayList<>();
+    List<Turismo> lista_turismo = new ArrayList<>();
+    List<Astro> lista_astro = new ArrayList<>();
+
+    public Terminal_Omnibus(String nombre, String direccion){
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    // Agregar Pasajero
+    public void Add_ListEspera(int id, String[] destinos){
+        Lista_Espera lista = new Lista_Espera(id, destinos);
+        lista_espera.add(lista);
+    }
+    public void Add_ListOficial(int id, String dia_salida, String dia_llegada){
+        Lista_Oficial lista = new Lista_Oficial(id, dia_salida, dia_llegada);
+        lista_oficial.add(lista);
+    }
+
+    //Agregar Omnibus
+    public void Add_ListTurismo(String chapa, String destino, String chofer, int cant_asientos, float km_recorridos, String hora_llegada){
+        Turismo lista = new Turismo(chapa, destino, chofer, cant_asientos, km_recorridos, hora_llegada);
+        lista_turismo.add(lista);
+    }
+
+    public void Add_ListAstro(String chapa, String destino, String chofer, int cant_asientos, float km_recorridos, String dia_salida, String hora_salida){
+        Astro lista = new Astro(chapa, destino, chofer, cant_asientos, km_recorridos, dia_salida, hora_salida);
+        lista_astro.add(lista);
+    }
+
     public abstract class Omnibus {
         
         protected String chapa;
@@ -75,6 +113,22 @@ public class Terminal_Omnibus {
         public float Precio(){
             return km_recorridos * 4;
         }
+
+        public String getHora_salida() {
+            return hora_salida;
+        }
+
+        public void setHora_salida(String hora_salida) {
+            this.hora_salida = hora_salida;
+        }
+
+        public String getDia_salida() {
+            return dia_salida;
+        }
+
+        public void setDia_salida(String dia_salida) {
+            this.dia_salida = dia_salida;
+        }
     }
 
     public class Turismo extends Omnibus {
@@ -101,6 +155,74 @@ public class Terminal_Omnibus {
             }
 
             return precio;
+        }
+
+        public String getHora_llegada() {
+            return hora_llegada;
+        }
+
+        public void setHora_llegada(String hora_llegada) {
+            this.hora_llegada = hora_llegada;
+        }
+    }
+
+    public abstract class Listas{
+        protected int id;
+
+        public Listas(int id){
+            this.id = id;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+    }
+
+    public class Lista_Espera extends Listas{
+        private String[] destinos;
+
+        public Lista_Espera(int id, String[] destinos){
+            super(id);
+            this.destinos = destinos;
+        }
+
+        public String[] getDestinos() {
+            return destinos;
+        }
+
+        public void setDestinos(String[] destinos) {
+            this.destinos = destinos;
+        }
+    }
+
+    public class Lista_Oficial extends Listas{
+        private String dia_salida;
+        private String destino;
+
+        public Lista_Oficial(int id, String dia_salida, String destino){
+            super(id);
+            this.destino = destino;
+            this.dia_salida = dia_salida;
+        }
+
+        public String getDia_salida() {
+            return dia_salida;
+        }
+
+        public void setDia_salida(String dia_salida) {
+            this.dia_salida = dia_salida;
+        }
+
+        public String getDestino() {
+            return destino;
+        }
+
+        public void setDestino(String destino) {
+            this.destino = destino;
         }
     }
 }
